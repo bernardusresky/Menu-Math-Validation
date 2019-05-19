7fd6beb1d822b7457d1bd814be929fa616570fac
Bernardus Rezky Suwandi <bernardusresky@gmail.com>
2019-05-19 12:13:13 +1000

    Add Init
    - Add init
    - Fix main method

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from calculation import Math
 
 class Menu():
 
+    def __init__(self):
+        self.math = Math()
+
 #This function is for printing the menu and let user choose from it.
     def menu(self):
         #Use while(True) for menu looping
@@ -22,25 +25,25 @@ class Menu():
             #Using If .. Elif .. Else statement/conditional check to check user input and run the matched funtion.
             if(userInput == "1"):
                 #When user input 1 it will ask for user to input 2 number and then call the sum function
-                Math.sum(self)
+                self.math.sum()
             elif(userInput == "2"):
                 #When user input 2 it will ask for user to input 2 number and then call the division function
-                Math.division(self)
+                self.math.division()
             elif(userInput == "3"):
                 #When user input 3 it will ask for user to input 2 number and then call the multiplication function
-                Math.multiplication(self)
+                self.math.multiplication()
             elif(userInput == "4"):
                 #When user input 4 it will ask for user to input 2 number and then call the division fraction function
-                Math.division_fractional(self)
+                self.math.division_fractional()
             elif(userInput == "5"):
                 #When user input 5 it will ask for user to input 2 number and then call the remainder function
-                Math.remainder(self)
+                self.math.remainder()
             elif(userInput == "6"):
                 #When user input 6 it will ask for user to input 2 number and then call the squeare function
-                Math.square(self)
+                self.math.square()
             elif(userInput == "7"):
                 #When user input 6 it will ask for user to input 2 number and then call the squeare function
-                Math.substraction(self)
+                self.math.substraction()
             elif(userInput == "8"):
                 #When user input 8 then it will the line and use break to stop the menu looping/ exit the program.
                 print("\n -------------- Thankyou! See you! -------------------")
@@ -49,8 +52,9 @@ class Menu():
                 #When user input other than 1 - 8 it will print this line
                 print("Invalid input - please try again.")
 
-    def main(self):
-        self.menu()
+def main():
+    menu = Menu()
+    menu.menu()
 
 if __name__ == '__main__':
-    Menu().main()
+    main()
